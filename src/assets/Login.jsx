6cfebb8ce07452ae83f180779218213a4937bfb3
Klevis <klevi.dervishi@gmail.com>
2024-03-30 12:23:58 +0200

    Fixes in dependencies

--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -1,31 +1,45 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { useState } from 'react'
 function Login(){
-    function login(){
-        console.log("hello")
+    let navigate = useNavigate()
+    const [error, setError] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const login = (e) =>{
+        e.preventDefault();
+        if (username != ""){
+            if(password !=""){
+                navigate('/home')
+            }else{
+                setError('Please fill the fields below')
+            }
+        }else{
+            setError('Please fill the fields below')
+        }
     }
     return(
         <div className='LogIn'>
-            <div className='image'></div>
             <div className='logInFormBox'>
                 <div action="#" className='logInBox'>
                     <div className='LoginTitle'>
                         <h1>Agro Pulse</h1>
                     </div>
-                    <form action="" className='logInForm'>
+                    <form className='logInForm'>
+                        <p className='error'>{error}</p>
                         <label htmlFor="username">
                             Username
                         </label>
                         <br />
-                        <input type="text" id='username' className='usernameInput'/>
+                        <input type="text" id='username' className='usernameInput' onChange={(e)=> setUsername(e.target.value)}/>
                         <br />
                         <label htmlFor="password">
                             Password
                         </label>
                         <br />
-                        <input type="password" id='password' className='usernameInput'/>
+                        <input type="password" id='password' className='usernameInput' onChange={(e)=> setPassword(e.target.value)}/>
                         <br />
-                        <button className='logInButton' onClick={login}>Log In</button>
+                        <button type='submit' className='logInButton' onClick={login}>Log In</button>
                         <p className='signUpFunction'>If you dont have an account <Link className='linkLogIn' to='/signIn'>Sign Up</Link></p>
                     </form>
                 </div>
--- src/assets/SignIn.jsx
+++ src/assets/SignIn.jsx
@@ -1,30 +1,43 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom'
+import { useState } from 'react'
 function SignIn(){
     let navigate = useNavigate();
-    function signIn(){
-        navigate('/home')
+    const [error, setError] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const signIn = (e) =>{
+        e.preventDefault();
+        if (username != ""){
+            if(password !=""){
+                navigate('/home')
+            }else{
+                setError('Please fill the fields below')
+            }
+        }else{
+            setError('Please fill the fields below')
+        }
     }
     return(
         <div className='LogIn'>
-            <div className='image'></div>
             <div className='logInFormBox'>
                 <div action="#" className='logInBox'>
                     <div className='LoginTitle'>
                         <h1>Agro Pulse</h1>
                     </div>
                     <form action="" className='logInForm'>
+                        <p className='error'>{error}</p>
                         <label htmlFor="username">
                             Username
                         </label>
                         <br />
-                        <input type="text" id='username' className='usernameInput' required/>
+                        <input type="text" id='username' className='usernameInput' onChange={(e)=> setUsername(e.target.value)} required/>
                         <br />
                         <label htmlFor="email">
                             Email
                         </label>
                         <br />
-                        <input type="email" id='email' className='usernameInput' required/>
+                        <input type="email" id='email' className='usernameInput' onChange={(e)=> setPassword(e.target.value)} required/>
                         <br />
                         <label htmlFor="password">
                             Password
