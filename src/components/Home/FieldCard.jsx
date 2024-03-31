548cb99d9e35dc9a10fab1ef59cb3a985834d1e4
Klevis <klevi.dervishi@gmail.com>
2024-03-31 06:51:34 +0300

    Makes minor changes

--- src/components/Home/FieldCard.jsx
+++ src/components/Home/FieldCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 
 const FieldCard = (props) => {
-    const {location, area, crop} = props.data;
+    const {country, area, crop} = props.data;
     const path = `/home/field/${props.data.id}`;
     return(
         <Link to={path} className="field-card" style={{textDecoration: "none", color: "black"}}>
@@ -13,7 +13,7 @@ const FieldCard = (props) => {
                 </div>
                 <div className="field-details">
                     <p style={{color:"#4f6f52"}}>Location</p>
-                    <h1>{location}</h1>
+                    <h1>{country}</h1>
                     <p style={{color:"#4f6f52"}}>Area</p>
                     <h2>{area}</h2>
                     <p style={{color:"#4f6f52"}}>Current Crop</p>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,14 +14,19 @@ function Home(){
     const navigate = useNavigate();
     const [expanded, setExpaned] = useState(false); 
     const [loaded, setLoaded] = useState(false);
-    const [content, setContent] = useState({}); 
+    const [dataSet, setDataSet] = useState({}); 
 
     useEffect(()=>{
         if(cookies.get('name') == undefined){
             navigate('/')
         }
-
-        setTimeout(()=>{setLoaded(true)}, 1000);
+        fetch('http://localhost:5000/farmerDataGet',{mode:"cors", method:"GET", headers:{'Content-Type':'application/json'}})
+        .then(result=>result.json())
+        .then(result => {
+            setDataSet(result)
+            console.log(result)
+            setLoaded(true)
+        })
     }, [])
 
     function refreshPage() {
@@ -29,39 +34,7 @@ function Home(){
         window.location.reload(false);
     }
 
-    const dataSet = [
-            {
-                id: 1,
-                location: "Kampala",
-                area: "10",
-                crop: "Rice"
-            },
-            {
-                id: 2,
-                location: "Durres",
-                area: "22",
-                crop: "Maize"
-            },
-            {
-                id:3,
-                location: "Kigali",
-                area: "15",
-                crop: "Beans"
-            },
-            {
-                id: 4,  
-                location: "Kigali",
-                area: "15",
-                crop: "Beans"
-            },
-            {
-                id: 5,
-                location: "Kigali",
-                area: "15",
-                crop: "Beans"
-            }
-        ]
-
+   
     return(
         <div className="home">
             { expanded == true  && 
